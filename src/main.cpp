e9bbc807aeee12151237b5d4bc871c9cc9dd71ab
sphurlsmith <sphurlsmith@gmail.com>
2024-09-06 19:11:02 +0300

    Stable version, but no projection in sight. Sorry!

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void rend(){
   shawa.renderMesh();
 
   wc_Object sha3d(NULL, 1, {0, 0, 3}, {0, 0, 0}, shawa);
-  wc_Camera cam(800, 600, 50*nsproj::DEGTORAD, 1, 10, {0,0,0}, {0,0,0});
+  wc_Camera cam(800, 600, 50*nsproj::DEGTORAD, 1, 10, {0,0,0}, {20*nsproj::DEGTORAD,0,0});
 
   cam.renderObject(sha3d, def3d);
 }
