0b7767db37b7252eb9620bc3ab62b2b21ea71bf5
sphurlsmith <sphurlsmith@gmail.com>
2024-09-06 19:19:42 +0300

    The Shader was complaining of a vec3 assigned to a vec2, but then when I solved the issue, the mesh disappeared. Even changing rotation to default didnt change anything. I'm suspecting we were not feeding in the matrices correctly?

--- shd/wc_vertex_3d.glsl
+++ shd/wc_vertex_3d.glsl
@@ -2,7 +2,7 @@
 
 layout (location=0) in vec3 vp;
 layout (location=1) in vec3 vc;
-layout (location=2) in vec3 vt;
+layout (location=2) in vec2 vt;
 
 out vec3 voc;
 out vec2 vtx;
@@ -12,8 +12,7 @@ uniform mat4 view;
 uniform mat4 proj;
 
 void main(){
-     gl_Position=vec4(vp, 1);
-     gl_Position*=mod*view*proj;
+     gl_Position=vec4(vp, 1)*mod*view*proj;
 
      voc=vc;
      vtx=vt;
